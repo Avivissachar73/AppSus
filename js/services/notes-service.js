0b59982eb063fc47d1c493d0a0dd9f817540c46e
Aviv Issachar <Avivissachar73@gmail.com>
2019-11-06 12:40:28 +0200

    making storage a-sinchronic

--- js/services/notes-service.js
+++ js/services/notes-service.js
@@ -2,8 +2,48 @@
 
 import utils from './util-service.js';
 
+const NOTES_STOREGE_KEY = 'my_notes';
+
 export default {
-    
+    getNotes
 };
 
 console.log(utils.getRandomId());
+
+var gNotes;
+
+
+function getNotes() {
+    return new Promise((resolve, reject) =>  {
+        utils.loadFromLocalStorage(NOTES_STOREGE_KEY)
+            .then(notes => {
+                gNotes = notes;
+                resolve(gNotes);
+            })
+            .catch(err => {
+                console.log(err);
+                gNotes = someNotes;
+                resolve(gNotes);
+            })
+    })
+}
+
+var someNotes = [
+    {
+        type: 'txt',
+        title: 'my note',
+        txt: 'some note',
+        fontColor: 'balck',
+        fontFamily: 'Arial',
+    },
+    {
+        type: 'picture',
+        title: 'my picture',
+        url: 'http://vignette1.wikia.nocookie.net/marveldatabase/images/a/a9/Spider-Man_Vol_1_1.jpg/revision/latest?cb=20080331205551'
+    },
+    {
+        type: 'video',
+        title: 'my video',
+        url: 'https://www.youtube.com/embed/otrH5hxJ2GE'
+    },
+];
